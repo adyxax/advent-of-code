5253b7c559f83be68026782b37fa6ddc3e79609b
Julien Dessaux <julien.dessaux@adyxax.org>
2021-12-08 15:47:01 +0100

    Added solutions for 6th day

---
+++ 2021/06/first.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	g := make([]int, 9)
+
+	s := bufio.NewReader(os.Stdin)
+	for {
+		buf := make([]byte, 2)
+		if n, _ := s.Read(buf); n == 0 {
+			break
+		}
+		g[buf[0]-'0']++
+	}
+	for d := 0; d <= 80; d++ {
+		fmt.Printf("%+v\n", g)
+		n := g[0]
+		for i := 0; i < 8; i++ {
+			g[i] = g[i+1]
+		}
+		g[6] += n
+		g[8] = n
+	}
+	total := 0
+	for i := 0; i < 8; i++ {
+		total += g[i]
+	}
+	fmt.Printf("%d\n", total)
+}
