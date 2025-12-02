4480966d63831bcd114000d457e967a6923038d0
Julien Dessaux <julien.dessaux@adyxax.org>
2025-12-02 09:53:07 +0100

    2025-02 in haskell

---
+++ 2025/02-Gift_Shop/first.hs
@@ -0,0 +1,55 @@
+-- requires cabal install --lib megaparsec parser-combinators heap vector
+module Main (main) where
+
+import           Control.Monad        (void, when)
+import qualified Data.List            as L
+import           Data.Void            (Void)
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+
+import Debug.Trace
+
+exampleExpectedOutput :: Int
+exampleExpectedOutput = 1227775554
+
+type Range = (Int, Int)
+
+type Input = [Range]
+type Parser = Parsec Void String
+
+parseNumber :: Parser Int
+parseNumber = read <$> some digitChar <* optional hspace
+
+parseRotation :: Parser Range
+parseRotation = (,) <$> parseNumber <* char '-'
+                    <*> parseNumber
+
+parseInput' :: Parser Input
+parseInput' = some (parseRotation <* optional (char ',')) <* optional eol <* eof
+
+parseInput :: String -> IO Input
+parseInput filename = do
+  input <- readFile filename
+  case runParser parseInput' filename input of
+    Left bundle  -> error $ errorBundlePretty bundle
+    Right input' -> return input'
+
+compute :: Input -> Int
+compute = sum . map compute'
+  where
+    compute' :: (Int, Int) -> Int
+    compute' (l, h) = sum $ map computeOne [l..h]
+    computeOne :: Int -> Int
+    computeOne n | even l && take h s == drop h s = n
+                 | otherwise = 0
+      where s = show n
+            l = length s
+            h = l `div` 2
+
+main :: IO ()
+main = do
+  example <- parseInput "example"
+  let exampleOutput = compute example
+  when  (exampleOutput /= exampleExpectedOutput)  (error $ "example failed: got " ++ show exampleOutput ++ " instead of " ++ show exampleExpectedOutput)
+  input <- parseInput "input"
+  print $ compute input
